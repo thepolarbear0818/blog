583172aedf9cdeb5a2de2542f33774c83410a87e
Malte Jürgens <maltejur@dismail.de>
2021-12-08 17:31:28 +0100

    nextra fixes

--- next.config.js
+++ next.config.js
@@ -2,13 +2,8 @@ const withNextra = require("nextra")({
   theme: "nextra-theme-docs-patch-maltejur",
   themeConfig: "./theme.config.tsx"
 });
-const withTM = require("next-transpile-modules")([
-  "nextra-theme-docs-patch-maltejur"
-]);
 
-module.exports = withTM(
-  withNextra({
-    reactStrictMode: true,
-    trailingSlash: true
-  })
-);
+module.exports = withNextra({
+  reactStrictMode: true,
+  trailingSlash: true
+});
